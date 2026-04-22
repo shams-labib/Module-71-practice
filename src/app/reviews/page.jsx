e827b3cde843bea962a5affb8f555eadc570e76e
shams al labib <samslabib19@gmail.com>
2026-04-22 23:56:26 +0600

    added

---
+++ src/app/reviews/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReviewsPage from "./ReviewPage";
+
+export const metadata = {
+  title: "Review Page",
+  description: "The best fast food noakhali",
+};
+
+const page = () => {
+  return (
+    <div>
+      <ReviewsPage />
+    </div>
+  );
+};
+
+export default page;
